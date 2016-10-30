2c8008563c116bb39b4d50247fab728914f5904e
Spencer <seaston@ccsknights.org>
2016-10-30 16:57:36 -0400

    Fix default message

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 var gulp = require('gulp');
+var exec = require('child_process').exec;
 
-gulp.task('default', function() {
+
+gulp.task('build', function() {
     gulp.src('src/client/*')
         .pipe(gulp.dest('public'))
+});
+
+gulp.task('deploy', function (cb) {
+    exec('firebase deploy', function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
 });
