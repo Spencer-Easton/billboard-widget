849539511a98628d785e4ed0e764e200aeb0eac5
seaston <seaston@ccsknights.org>
2016-11-07 15:29:39 -0500

    Banner Change

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
 var gulp = require('gulp');
+var minify = require('gulp-minify');
+var htmlmin = require('gulp-htmlmin');
 var exec = require('child_process').exec;
 
 
-gulp.task('build', function() {
-    gulp.src('src/client/*')
+gulp.task('jsMin', function() {
+    gulp.src('src/client/*.js')
+        .pipe(minify())
         .pipe(gulp.dest('public'))
 });
+
+gulp.task('htmlMin', function(){
+    gulp.src('src/client/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('public'));
+});
+
+
+gulp.task('build', ['jsMin','htmlMin']);
 
 gulp.task('deploy', function (cb) {
     exec('firebase deploy', function (err, stdout, stderr) {
